1930a375d74b31e91ecaa878346ca55a5aaeee14
Piyush Acharya <acharyapiyush1@outlook.com>
2022-10-06 17:18:48 -0700

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,34 +1,16 @@
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums.toString().contains(String.valueOf(target - nums[i]))) {
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[j] == (target - nums[i])) {
-                        int[] return_list = { i, j };
-                        return return_list;
-                    }
-                }
-            }
-        }
-        return nums;
-    }
-
-    public int[] twoSumSolution(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
+            int num1 = nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+                int num2 = nums[j];
+                if (num1 + num2 == target) {
+                    int[] output = { i, j };
+                    return output;
                 }
             }
         }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        int[] inputlist = { 3,3 };
-        int[] outputlist = twoSum(inputlist, 6);
-        for (int outputnum : outputlist) {
-            System.out.println(outputnum);
-        }
+        int[] output = {};
+        return output;
     }
 }
