f8cf1f4c5fafc30de89e743fc7026eeb085c3d99
Piyush Acharya <acharyapiyush1@outlook.com>
2022-10-19 19:20:19 -0700

    Added Add Two Integers Files

---
+++ 2235. Add Two Integers/Solution.md
@@ -0,0 +1,27 @@
+# Add Two Integers
+
+## Intuition
+
+I first looked at the inputs that are given to the program. I walked through a simple version of the problem in my head and then in pseudocode.
+
+## Approach
+
+I take the 2 inputs, add them together, and return the sum.
+
+## Complexity
+
+- Time complexity:
+O(1)
+
+- Space complexity:
+O(1)
+
+## Code
+
+```java
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+```
--- Two Sum.java
+++ Two Sum.java
@@ -1,11 +1,11 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) 
-        {
-            if (map.containsKey(target-nums[i]))
-            {
-                return new int[] {i, map.get(target-nums[i])};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { i, map.get(target - nums[i]) };
             }
             map.put(nums[i], i);
         }
