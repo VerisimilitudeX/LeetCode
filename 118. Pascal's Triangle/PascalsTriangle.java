cc524772ef49440b96af9dc6ade4c4cfe61f90d8
Piyush Acharya <acharyapiyush1@outlook.com>
2023-05-25 17:56:03 -0700

    Pascal's Triangle

---
+++ 118. Pascal's Triangle/PascalsTriangle.java
@@ -0,0 +1,18 @@
+public class PascalsTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allRows = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(i);
+            String num = String.valueOf(Math.pow(11, i));
+            for (char c : num.toCharArray()) {
+                row.add(Integer.parseInt(String.valueOf(c)));
+            }
+            allRows.add(row);
+        }
+        return allRows;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PascalsTriangle.generate(5));
+    }
+}
